1b7a7a3a09885708874f5ab4f51d7d0ecd76e38b
tomHuang <tom.huang@positivegrid.com>
2018-04-10 15:20:25 +0800

    chore: add some verbose
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -18,6 +18,8 @@ def load_image(path):
   img = io.imread(path)
   img_lab = color.rgb2lab(img)
   img_grey = color.rgb2grey(img)
+  print(img_grey)
+  print(img_grey.shape)
   img_shape = img.shape
   img = img.reshape((-1,3))
   img_lab = img_lab.reshape((-1,3))
@@ -44,14 +46,19 @@ if __name__ == "__main__":
     os.makedirs(out_dir)
 
   filter_bank = sio.loadmat(filter_bank_file_path)['F']
+  label_colors = np.random.randint(256, size=(kmeans_num_clusters_color_seg, 3))
 
   for img_name in problem2_img_names:
-    label_colors = np.random.randint(256, size=(kmeans_num_clusters_color_seg, 3))
+    print('Start processing ' + img_name)
     img, img_lab, img_grey, img_shape = load_image(problem2_dir + img_name)
+    print('Filtering...')
     img_filtered = filter_image(img_grey, filter_bank, img_shape)
+    print(img_filtered.shape)
+    print('Doing segmentation...')
     seg = seg_image(img, kmeans_num_clusters_color_seg, label_colors)
     seg_lab = seg_image(img_lab, kmeans_num_clusters_color_seg, label_colors)
     seg_filtered = seg_image(img_filtered, kmeans_num_clusters_texture_seg, label_colors)
+    print('Saving image...\n\n')
     save_image(seg, out_dir + '/seg_rgb_' + img_name, img_shape)
     save_image(seg_lab, out_dir + '/seg_lab_' + img_name, img_shape)
     save_image(seg_filtered, out_dir + '/seg_filtered_' + img_name, img_shape)
