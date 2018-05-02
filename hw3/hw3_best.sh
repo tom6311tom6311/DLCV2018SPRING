82a3ab01e84eb8a15c7b68083c958de4087fc4a2
tomHuang <tom.huang@positivegrid.com>
2018-05-02 18:37:00 +0800

    feat: add test script
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

---
+++ hw3/hw3_best.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $1 ]; then
+  img_dir=$1
+else
+  img_dir="data/validation"
+fi
+
+if [ $2 ]; then
+  out_dir=$2
+else
+  out_dir="out"
+fi
+
+mkdir -p model
+wget https://www.dropbox.com/s/n8rfs1dmgebyrsw/best.hdf5?dl=1 -O model/best.hdf5
+python3 test.py 0 False model/best.hdf5 $img_dir $out_dir
