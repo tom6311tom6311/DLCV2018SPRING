99d21618f9ccb43c075dc38f12f8b93c33eefc0c
tomHuang <tom.huang@positivegrid.com>
2018-05-08 16:04:03 +0800

    fix: save chk point error
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- hw4/1_vae/train.py
+++ hw4/1_vae/train.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import shutil
 from model.SimpleAE import SimpleAE
 import util
 import tensorflow as tf
@@ -8,7 +9,8 @@ from keras.backend.tensorflow_backend import set_session
 
 TRAIN_DATA_DIR = '../data/train/'
 TEST_DATA_DIR = '../data/test/'
-OUTPUT_MODEL_PATH_PREFIX = 'out/simple_'
+OUTPUT_MODEL_DIR = 'out_simple/'
+OUTPUT_MODEL_PATH_PREFIX = OUTPUT_MODEL_DIR + 'simple_'
 ENC_DIM = 1024
 MAX_EPOCHS = 100
 BATCH_SIZE = 64
@@ -19,6 +21,11 @@ config.gpu_options.per_process_gpu_memory_fraction = 0.8
 set_session(tf.Session(config=config))
 
 if __name__ == '__main__':
+  if not os.path.exists(OUTPUT_MODEL_DIR):
+    os.makedirs(OUTPUT_MODEL_DIR)
+  else:
+    shutil.rmtree(OUTPUT_MODEL_DIR)
+    os.makedirs(OUTPUT_MODEL_DIR)
   print('Loading training data...')
   train_data = util.load_data(TRAIN_DATA_DIR)
   print('\nLoading testing data...')
@@ -33,7 +40,7 @@ if __name__ == '__main__':
     test_data,
     epochs=MAX_EPOCHS,
     batch_size=BATCH_SIZE,
-    out_model_prefix='../' + OUTPUT_MODEL_PATH_PREFIX
+    out_model_prefix=OUTPUT_MODEL_PATH_PREFIX
   )
   print('\nfinished with models saved.')
 
