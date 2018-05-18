b9dba9a5ec1fc149792137217226c455153bd266
tomHuang <tom.huang@positivegrid.com>
2018-05-18 23:51:21 +0800

    feat: update hw4 sh
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- hw4/hw4.sh
+++ hw4/hw4.sh
@@ -16,7 +16,9 @@ mkdir -p model
 wget https://www.dropbox.com/s/3z5qujt6215tt93/vae_30.hdf5?dl=1 -O model/vae.hdf5
 wget https://www.dropbox.com/s/55mxr3mvosakvz4/dcgan_24_gen?dl=1 -O model/dcgan.hdf5
 wget https://www.dropbox.com/s/0w3ripyhiu1kog0/acgan_34_gen?dl=1 -O model/acgan.hdf5
-python 1_vae/fig1_3.py 0 ../model/vae.hdf5 $in_dir $out_dir
-python 1_vae/fig1_4.py 0 ../model/vae.hdf5 $in_dir $out_dir
-python 2_gan/fig2_3.py 0 ../model/dcgan.hdf5 $in_dir $out_dir
-python 2_gan/fig3_3.py 0 ../model/acgan.hdf5 $in_dir $out_dir
+cd 1_vae
+python fig1_3.py 0 ../model/vae.hdf5 $in_dir $out_dir
+python fig1_4.py 0 ../model/vae.hdf5 $in_dir $out_dir
+cd ../2_gan
+python fig2_3.py 0 ../model/dcgan.hdf5 $in_dir $out_dir
+python fig3_3.py 0 ../model/acgan.hdf5 $in_dir $out_dir
