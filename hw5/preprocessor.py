97e7dd25adb5b2274a7ef7109c0a5ec2b6e00231
tomHuang <tom.huang@positivegrid.com>
2018-06-02 16:40:55 +0800

    feat: average pooling of frame feats
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- hw5/preprocessor.py
+++ hw5/preprocessor.py
@@ -19,15 +19,20 @@ def progress(count, total, suffix=''):
   sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
   sys.stdout.flush()
 
-def extract_feats(is_train=True):
+def extract_feats(is_train=True, merge_frames=True):
   mode = 'train' if is_train else 'valid'
   video_list = reader.getVideoList(VIDEO_PATH + 'label/gt_' + mode + '.csv')
   all_frames = []
   all_labels = []
   for idx in range(len(video_list['Video_index'])):
     frames = reader.readShortVideo(VIDEO_PATH + 'video/' + mode, video_list['Video_category'][idx], video_list['Video_name'][idx])
-    all_frames = all_frames + frames
-    all_labels = all_labels + [video_list['Action_labels'][idx]] * len(frames)
+    if merge_frames:
+      frames = np.average(frames, 0)
+      all_frames.append(frames)
+      all_labels.append(video_list['Action_labels'][idx])
+    else:
+      all_frames = all_frames + frames
+      all_labels = all_labels + [video_list['Action_labels'][idx]] * len(frames)
     progress(idx+1, len(video_list['Video_index']))
   all_frames = np.array(all_frames).astype(np.float64)
   all_frames = preprocess_input(all_frames)
