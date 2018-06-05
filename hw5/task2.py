786398044e2ea6e09dad1cd1fcdc8526cfd63f42
tomHuang <tom.huang@positivegrid.com>
2018-06-05 16:13:00 +0800

    chore: tune rnn structure
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- hw5/task2.py
+++ hw5/task2.py
@@ -5,7 +5,7 @@ import numpy as np
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Conv1D, Flatten, LSTM
+from keras.layers import Dense, Dropout, Conv1D, Flatten, LSTM, Bidirectional
 from keras.callbacks import EarlyStopping
 from keras import regularizers
 
@@ -26,8 +26,11 @@ print(train_feats.shape)
 print(train_labels.shape)
 
 classifier = Sequential()
-classifier.add(LSTM(256, dropout=0.2, input_shape=train_feats.shape[1:]))
-classifier.add(Dense(1024, activation='relu'))
+# classifier.add(LSTM(8, return_sequences=True, dropout=0.3, input_shape=train_feats.shape[1:]))
+# classifier.add(LSTM(8, dropout=0.3))
+classifier.add(Bidirectional(LSTM(32, return_sequences=True, dropout=0.3), input_shape=train_feats.shape[1:]))
+classifier.add(Bidirectional(LSTM(32, dropout=0.3)))
+# classifier.add(Dense(128, activation='relu'))
 classifier.add(Dense(11, activation='softmax'))
 classifier.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
