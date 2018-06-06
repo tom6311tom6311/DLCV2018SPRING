1e49d0d471a653aa44519e0221a13013d40441ab
tomHuang <tom.huang@positivegrid.com>
2018-06-06 10:01:37 +0800

    feat: task2 batch training script
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

---
+++ hw5/task2_batch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python3 task2.py 2 data/TrimmedVideos/feat3_2048_True/ > log_feat3_2048_True.txt
+python3 task2.py 2 data/TrimmedVideos/feat3_2048_False/ > log_feat3_2048_False.txt
+python3 task2.py 2 data/TrimmedVideos/feat5_2048_True/ > log_feat5_2048_True.txt
+python3 task2.py 2 data/TrimmedVideos/feat5_2048_False/ > log_feat5_2048_False.txt
+python3 task2.py 2 data/TrimmedVideos/feat6_2048_True/ > log_feat6_2048_True.txt
+python3 task2.py 2 data/TrimmedVideos/feat6_2048_False/ > log_feat6_2048_False.txt
+python3 task2.py 2 data/TrimmedVideos/feat8_2048_True/ > log_feat8_2048_True.txt
+python3 task2.py 2 data/TrimmedVideos/feat8_2048_False/ > log_feat8_2048_False.txt
+python3 task2.py 2 data/TrimmedVideos/feat10_2048_True/ > log_feat10_2048_True.txt
+python3 task2.py 2 data/TrimmedVideos/feat10_2048_False/ > log_feat10_2048_False.txt
+python3 task2.py 2 data/TrimmedVideos/feat20_2048_True/ > log_feat20_2048_True.txt
+python3 task2.py 2 data/TrimmedVideos/feat20_2048_False/ > log_feat20_2048_False.txt
